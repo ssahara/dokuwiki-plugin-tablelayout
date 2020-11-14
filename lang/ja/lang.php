97c6ecf9716052e8b7ad24105a521d9e38c01def
Satoshi Sahara <sahara.satoshi@gmail.com>
2020-11-15 01:15:30 +0900

    Japanese language files

---
+++ lang/ja/lang.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Japanese language file for tablelayout plugin
+ *
+ * @author Satoshi Sahara <sahara.satoshi@gmail.com>
+ */
+
+$lang['legend:tablelayout'] = 'テーブル レイアウト';
+$lang['button:apply'] = 'レイアウトのプレビュー';
+$lang['label:rowsHeader'] = '固定ヘッダーとする行数';
+$lang['label:rowsVisible'] = 'データ表示部の行数 (0 = all)';
+$lang['label:alignment'] = 'テーブル配置';
+$lang['label:tableSort'] = 'ソート機能';
+$lang['label:tableSearch'] = '行検索機能';
+$lang['label:tablePrint'] = '表印刷機能';
+$lang['option:default'] = 'デフォルト';
+$lang['option:float left'] = '左寄せ';
+$lang['option:float right'] = '右寄せ';
+$lang['option:center'] = 'センタリング';
+$lang['text:explain form'] = '［レイアウトのプレビュー］ボタンをクリックして確認してください';
+$lang['js']['loading'] = 'Loading';
+$lang['js']['print'] = '印刷';
+$lang['js']['search'] = '表内検索';
+
+//Setup VIM: ex: et ts=4 :
---
+++ lang/ja/settings.php
@@ -0,0 +1,3 @@
+<?php
+
+$lang['features_active_by_default'] = 'テーブルの新規作成時に、オプション機能（ソート、行検索、表印刷）を有効にする';
